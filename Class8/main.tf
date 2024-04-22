db342f44392ad7da5261c50c0993cbf6399da547
Aijan13 <13muratova@gmail.com>
2024-04-22 04:04:46 +0000

    new changes

--- Class8/main.tf
+++ Class8/main.tf
@@ -4,6 +4,7 @@ provider aws {
 
 module "vpc" {
     source = "Aijan13/vpcbatch5/aws"
+    version = "0.0.4"
     region = "us-east-2"
     vpc_cidr = "10.0.0.0/16"
     subnet1_cidr = "10.0.1.0/24"
@@ -14,4 +15,11 @@ module "vpc" {
     subnet1_name = "hello1"
     subnet2_name = "hello2"
     subnet3_name = "hello3"
-} 
+    ports = [
+        {from_port = 22, to_port = 22},
+        {from_port = 80, to_port = 80},                #Provide list of ports
+        {from_port = 81, to_port = 81},
+        {from_port = 82, to_port = 82}
+      
+    ]
+}
